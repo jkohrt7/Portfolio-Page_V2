c4f30d5fb091671d7fcc69b21bec4fe414e76583
Jared Kohrt <jkohrt7@gmail.com>
2021-09-28 11:44:43 -0400

    Added header, navbar, started cards

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
+      minWidth: {
+        "20": "20px"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
